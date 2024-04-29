06224bc7691d5b349fe412b0029e41b0581126d8
MVargasVigo <mauricio.vargas98@gmail.com>
2024-04-28 23:04:08 -0500

    faq accodion finalized

--- faq-accordion-main/index.js
+++ faq-accordion-main/index.js
@@ -5,8 +5,16 @@ btns.forEach((btn) => {
     const openBtn = document.querySelector(btn.dataset.openTarget);
     const closeBtn = document.querySelector(btn.dataset.closeTarget);
     const answer = document.querySelector(btn.dataset.answerTarget);
-    openBtn.classList.add("hidden");
-    closeBtn.classList.remove("hidden");
-    answer.classList.remove("hidden");
+    if (btn.classList.contains("active")) {
+      openBtn.classList.add("hidden");
+      closeBtn.classList.remove("hidden");
+      answer.classList.remove("hidden");
+      btn.classList.remove("active");
+    } else {
+      openBtn.classList.remove("hidden");
+      closeBtn.classList.add("hidden");
+      answer.classList.add("hidden");
+      btn.classList.add("active");
+    }
   });
 });
