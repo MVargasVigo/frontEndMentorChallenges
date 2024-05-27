b82ad4e131249cb6c4ed093c9e7d5dd9b0be3773
MVargasVigo <mauricio.vargas98@gmail.com>
2024-05-27 14:05:50 -0500

    15 scripts and styles finalized

--- 15-base-apparel-coming-soon-master/index.js
+++ 15-base-apparel-coming-soon-master/index.js
@@ -1,8 +1,37 @@
 const input = document.getElementById("input");
 const button = document.getElementById("btn");
+const message = document.getElementById("message");
+const error = document.getElementById("error");
 
 button.addEventListener("click", function () {
+  console.log("click");
+  const normalized = input.value.toLowerCase();
   if (!input.value) {
-    alert("Please provide a valid email");
+    emailError();
+  } else if (!normalized.includes("@")) {
+    emailError();
+  } else if (!normalized.includes(".com")) {
+    emailError();
+  } else {
+    error.classList.add("hidden");
+    emailReceived();
   }
 });
+
+const showMessage = function () {
+  message.classList.remove("hidden");
+};
+
+const emailReceived = function () {
+  message.textContent = "Thank you for adding your email! We will be in touch.";
+  message.style.color = "hsl(0, 36%, 70%)";
+  message.style.fontWeight = "600";
+  showMessage();
+  input.value = "";
+};
+
+const emailError = function () {
+  showMessage();
+  error.classList.remove("hidden");
+  input.value = "";
+};
