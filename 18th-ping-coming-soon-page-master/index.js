4b4d8eeb9465f1b505d45a5895459b1c9ac97e69
MVargasVigo <mauricio.vargas98@gmail.com>
2024-05-30 23:52:26 -0500

    18 90% finalized

--- 18th-ping-coming-soon-page-master/index.js
+++ 18th-ping-coming-soon-page-master/index.js
@@ -5,15 +5,29 @@ const igLogo = document.getElementById("instagram");
 
 const button = document.getElementById("send");
 const input = document.querySelector("#submit input");
+const errorMsg = document.getElementById("errorText");
+
+const printError = function (message) {
+  errorMsg.textContent = message;
+  errorMsg.style.marginTop = "10px";
+  errorMsg.style.marginLeft = "10px";
+  errorMsg.style.color = "red";
+};
 
 button.addEventListener("click", function () {
   console.log(input.value);
   if (input.value.includes("@") && input.value.includes(".com")) {
+    input.style.color = "#4f7df3";
+    input.style.fontWeight = "700";
     console.log("Success!");
   } else {
+    input.style.borderColor = "red";
+    input.style.borderWidth = "2px";
     if (!input.value) {
+      printError("This field cannot be empty!");
       console.log("this field cannot be empty!");
     } else if (!input.value.includes("@") || !input.value.includes(".com"))
-      console.log("this is not an email!");
+      printError("This is not an email!");
+    console.log("this is not an email!");
   }
 });
