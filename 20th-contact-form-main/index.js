4e0118f3a17e187d1fdb814254762030a26063e3
MVargasVigo <mauricio.vargas98@gmail.com>
2024-06-06 23:50:46 -0500

    20th scripts 60%

--- 20th-contact-form-main/index.js
+++ 20th-contact-form-main/index.js
@@ -11,9 +11,10 @@ const redBorders = document.querySelectorAll(".redBorder");
 const generalRadio = document.getElementById("generalQ");
 const supportRadio = document.getElementById("supportQ");
 const consentCheck = document.getElementById("consent");
+const radioArray = [generalRadio, supportRadio, consentCheck];
 
 //Success Message
-const successMessage = document.getElementById("success");
+const successMessage = document.getElementById("success"); //agregar transicion al mensaje de success
 
 //Errors
 const allErrors = document.querySelectorAll(".error");
@@ -25,12 +26,19 @@ const messErr = document.getElementById("messErr");
 
 //Functions
 const inputChecker = function (input) {
-  if (input) return true;
+  if (input.value) return true;
   else return false;
 };
 
-const allInputChecker = function (inputArray) {};
+/* const allInputChecker = function (inputArray) {
+  inputArray.forEach((input) => {
+    if (inputChecker(input)) return true;
+    else return false;
+  });
+};
 
+console.log(allInputChecker(allInputs));
+ */
 const radioChecker = function (radio) {
   if (radio.checked) return true;
   else return false;
@@ -42,30 +50,47 @@ const emailChecker = function (email) {
     return true;
   } else return false;
 };
+
 const success = function () {
   console.log("Success!");
   successMessage.classList.remove("hidden");
-  allErrors.forEach((error) => {
-    if (!error.classList.contains("hidden")) {
-      error.classList.add("hidden");
-    }
-  });
+  errorHandler();
+  reset();
+};
+
+const reset = function () {
   allInputs.forEach((input) => (input.value = ""));
   textArea.value = "";
-  console.log(generalRadio.value);
-  generalRadio.checked = false;
-  supportRadio.checked = false;
-  consentCheck.checked = false;
+  radioArray.forEach((radio) => (radio.checked = false));
+};
+
+//State?
+let show = true;
+
+const errorHandler = function () {
+  if (show) {
+    allErrors.forEach((error) => error.classList.remove("hidden"));
+    redBorders.forEach((border) => (border.style.borderColor = "#d73c3c"));
+  } else {
+    allErrors.forEach((error) => error.classList.add("hidden"));
+    redBorders.forEach((border) => (border.style.borderColor = "#87a3a6"));
+  }
 };
 
 button.addEventListener("click", function (e) {
   e.preventDefault();
-  if (emailChecker(emailInput.value)) {
+  if (
+    inputChecker(firstNameInput) &&
+    inputChecker(lastNameInput) &&
+    emailChecker(emailInput.value) &&
+    inputChecker(textArea)
+  ) {
+    show = false;
     success();
   } /* else if () {
 
   } */ else {
-    allErrors.forEach((error) => error.classList.remove("hidden"));
-    redBorders.forEach((border) => (border.style.borderColor = "#d73c3c"));
+    show = true;
+    errorHandler();
   }
 });
